c231ef1bc01d4ed97df4f43955907ac1bbafc5d4
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-04-14 18:21:25 +0200

    Added content

---
+++ nav.js
@@ -0,0 +1,19 @@
+function $(elem) {
+  return document.querySelectorAll(elem);
+}
+
+function changeTo(dest) {
+  for (elem of $('div[data-page]')) {
+    elem.style.display =
+      elem.getAttribute('data-page') == dest ? 'block' : 'none';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  changeTo('list');
+  for (elem of $('nav > div')) {
+    elem.addEventListener('click', (e) => {
+      changeTo(e.currentTarget.getAttribute('data-dest'));
+    });
+  }
+});
