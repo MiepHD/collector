b442c2c578a1f433302da4c723e34534224d6f12
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-04-26 15:22:52 +0200

    Added data

--- data.js
+++ data.js
@@ -1,31 +1,45 @@
 data = [
   {
-    description: 'First place to find',
-  },
-  {
-    description: 'Lorem ipsum',
-    title: 'Titel',
-    question: 'Frage',
-    tipp: 'ABC',
+    description: 'Fähre von Dunkirk nach Dover',
+    title: 'Fähre von Dunkirk nach Dover',
+    question: 'Wie breit ist die Strasse von Dover, die engste Stelle des Ärmelkanals, swischen Grossbritammiem und Frankreich?',
+    tipp: 'Bei gutem Wetter kann man die andere Seite sehen',
     answers: [
       {
-        text: 'A',
-        bool: true,
-      },
-      {
-        text: 'B',
+        text: 'A) 90 km wie von Niedergörsdorf nach Berlin',
         bool: false,
       },
       {
-        text: 'C',
+        text: 'B) 73 km wie von Landshut nach München',
         bool: false,
       },
       {
-        text: 'D',
+        text: 'C) 34 km wie von Niedergörsdorf nach Wittenberg',
         bool: true,
       },
     ],
   },
-  {},
+
+  {
+    description: 'White Cliffs of Dover',
+    title: 'Weisse Klippen von Dover',
+    question: 'Grossbritannien war nicht immer eine Insel. Was ist passiert?',
+    tipp: 'Es passierte zum Ende der letzten Eiszeit',
+    answers: [
+      {
+        text: 'A) Ein Tsunami überspülte die Landverbindung',
+        bool: true,
+      },
+      {
+        text: 'B) ein Erdbeben brach das Land auseinander',
+        bool: false,
+      },
+      {
+        text: 'C) Ein riesiger Meteorit schlug ein und trennte das Land',
+        bool: false,
+      },
+    ],
+  },
+
   {},
 ];
