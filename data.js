05654ff74c402821bbea3a355704c49e3231dfdc
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-04-26 16:59:56 +0200

    Fixed nav and sizing

--- data.js
+++ data.js
@@ -27,6 +27,454 @@ data = [
       },
     ],
   },
-  {},
-  {},
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
+  {
+    description: 'Lorem ipsum',
+    title: 'Titel',
+    question: 'Frage',
+    tipp: 'ABC',
+    task: 'Take pic',
+    answers: [
+      {
+        text: 'A',
+        bool: true,
+      },
+      {
+        text: 'B',
+        bool: false,
+      },
+      {
+        text: 'C',
+        bool: false,
+      },
+      {
+        text: 'D',
+        bool: true,
+      },
+    ],
+  },
 ];
